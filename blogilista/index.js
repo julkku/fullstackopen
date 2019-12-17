8ddb8343d982610a241226a0806c0b3dbecd7c18
Julius Uusinarkaus <julius@uusinarkaus.com>
2019-12-17 20:08:07 +0200

    moved model to module

--- blogilista/index.js
+++ blogilista/index.js
@@ -3,18 +3,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
+const mongoose = require('mongoose')
 
+const Blog = require('./models/post.js')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
