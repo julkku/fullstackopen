d3f6610bd1ca4e3093e0740bba77c673db9269b0
Julius Uusinarkaus <julius@uusinarkaus.com>
2019-12-17 20:23:53 +0200

    4.2 finished

---
+++ blogilista/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const postsRouter = require('./controllers/posts')
+app.use('/api/blogs', postsRouter)
+
+module.exports = app
--- blogilista/index.js
+++ blogilista/index.js
@@ -1,23 +1,5 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const app = require('./app')
 const config = require('./utils/config')
-const mongoose = require('mongoose')
-
-const Blog = require('./models/post.js')
-
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-const postsRouter = require('./controllers/posts')
-app.use('/api/blogs', postsRouter)
-
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
